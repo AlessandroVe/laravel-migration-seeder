f537b1b09e55f5cb336ae108f8d61a175f08509d
AlessandroVe <aveneziano536@gmail.com>
2021-09-12 21:25:35 +0200

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,9 @@ class HomeController extends Controller
 {
     public function index(){
         $all_trips = trip::all();
-        return view('home',compact('all_trips'));
+        $trip_with_train = trip::where('medium','treno')->get();
+        $trip_with_airplane = trip::where('medium','aereo')->get();
+        
+        return view('home',compact('all_trips','trip_with_train','trip_with_airplane'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,6 @@
     <body>
         <h1>I nostri viaggi disponibili</h1>
         @foreach($all_trips as $trip)
-
             <div class="card">
                 <div>departure : {{$trip['departure']}}</div>
                 <div>arrival : {{$trip['arrivals']}}</div>
@@ -18,7 +17,31 @@
                 <div>medium :{{$trip['medium']}}</div>
                 <div>price :{{$trip['price']}}$</div>
             </div>
-
         @endforeach
+        
+        <h1>I nostri viaggi in treno</h1>
+        @foreach($trip_with_train as $trip)
+            <div class="card">
+                <div>departure : {{$trip['departure']}}</div>
+                <div>arrival : {{$trip['arrivals']}}</div>
+                <div>{{$trip['description']}}</div>
+                <div>distance:{{$trip['distance']}}km </div>
+                <div>medium :{{$trip['medium']}}</div>
+                <div>price :{{$trip['price']}}$</div>
+            </div>
+        @endforeach
+
+        <h1>I nostri viaggi in aereo</h1>
+        @foreach($trip_with_airplane as $trip)
+            <div class="card">
+                <div>departure : {{$trip['departure']}}</div>
+                <div>arrival : {{$trip['arrivals']}}</div>
+                <div>{{$trip['description']}}</div>
+                <div>distance:{{$trip['distance']}}km </div>
+                <div>medium :{{$trip['medium']}}</div>
+                <div>price :{{$trip['price']}}$</div>
+            </div>
+        @endforeach
+
     </body>
 </html>
